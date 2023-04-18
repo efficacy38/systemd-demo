678b1c117c3041cbe2fc714869e4eced66790afb
jerry <efficacy38@gmail.com>
2023-04-18 20:32:46 +0800

    feat: add the timer example

---
+++ timer-section/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# install the counter.py
+install *.sh /usr/local/bin/
+
+
+# install the systemd script
+install *.service /etc/systemd/system/
+install *.timer /etc/systemd/system/
+systemctl daemon-reload
+
+# clean up the test file
+mkdir -p /data
+echo "----" >> /data/TEST
+cat /data/TEST
+chmod 666 /data/TEST
