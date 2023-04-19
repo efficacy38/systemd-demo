40a5baf1b59f11ac5fb11e9de153072d626dc782
jerry <efficacy38@gmail.com>
2023-04-19 23:15:48 +0800

    update

---
+++ utils/systemd-email.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+/usr/bin/sendmail -t <<ERRMAIL
+To: $1
+From: systemd <root@$HOSTNAME>
+Subject: $2
+Content-Transfer-Encoding: 8bit
+Content-Type: text/plain; charset=UTF-8
+
+$(systemctl status --full "$2")
+ERRMAIL
+
